ce4a036b645779f53ef0729378650b2e54ac8aae
suvethagan <suvethagan1995@gmail.com>
2018-03-31 11:52:19 +0530

    check
    assignment

---
+++ check/Summery.java
@@ -0,0 +1,12 @@
+package Final;
+
+public class Summery {
+
+	public void showSaleSummery() {
+		ProductList ds = new ProductList();
+		ds.showSalesSummery();
+		// TODO Auto-generated method stub
+		
+	}
+
+}
